6a0b60bf4e9e26aa57dbc202496855f041ff550c
Danny <holler@dannyburns.me>
2019-03-24 12:14:08 -0700

    Basic Refinement and Prep

--- js/db-animate.js
+++ js/db-animate.js
@@ -1,19 +1,26 @@
-// Main JS
+/*!
+ * db-animate.css -http://dannyburns.me
+ * Version 1.0
+ * Licenced under MIT license  - http://opensource.org/licenses/MIT
+ *
+ * Copyright (c) 2019 Danny Burns
+ */
 
+//Page element animatin
 function aniMate(element) {
-    // Assign El
-    const el = document.querySelectorAll(element);
+  // Assign El
+  const el = document.querySelectorAll(element);
 
-    // Event Listener
-    window.addEventListener('scroll', function() {
-
-        el.forEach(elem => {
-            if (  elem.getBoundingClientRect().top < window.innerHeight / 2  ) {
-                elem.classList.add('animate');
-            }
-        });
+  // Event Listener
+  window.addEventListener("scroll", function() {
+    el.forEach(elem => {
+      if (elem.getBoundingClientRect().top < window.innerHeight / 2) {
+        elem.classList.add("animate");
+      }
     });
+  });
 }
-aniMate('.fade-in');
-aniMate('.fade-in-left');
-aniMate('.fade-in-right');
+aniMate(".fade-in");
+aniMate(".fade-in-left");
+aniMate(".fade-in-right");
+aniMate(".scale-down");
